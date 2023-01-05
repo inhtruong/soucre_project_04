5d038f096f5ca91c86d5ffccddff7fa0c3e9a29b
inhtruong <duydat.dhsp@gmail.com>
2023-01-05 22:59:54 +0700

    update pattern 5678

--- src/app/net/poweregg/mitsubishi/action/UMB01Bean.java
+++ src/app/net/poweregg/mitsubishi/action/UMB01Bean.java
@@ -44,127 +44,125 @@ public class UMB01Bean implements Serializable {
   private Umb01Dto umb01Dto;
 
   public void calculateValue() {
-      BigDecimal retailPrice = umb01Dto.getPriceRefDto().getRetailPrice();
-      BigDecimal unitPriceSmallParcel = umb01Dto.getPriceRefDto().getUnitPriceSmallParcel();
-      BigDecimal unitPriceForeheadColor = umb01Dto.getPriceRefDto().getUnitPriceForeheadColor();
-      BigDecimal primaryStoreOpenRate = umb01Dto.getPriceRefDto().getPrimaryStoreOpenRate();
-      BigDecimal primaryStoreCommissionAmount = umb01Dto.getPriceRefDto().getPrimaryStoreCommissionAmount();
-      BigDecimal secondStoreOpenRate = umb01Dto.getSecondStoreOpenRate();
-      BigDecimal secondStoreOpenAmount = umb01Dto.getSecondStoreOpenAmount();
-      BigDecimal lotQuantity = umb01Dto.getPriceRefDto().getLotQuantity();
-      BigDecimal calValue = new BigDecimal("0");
-      BigDecimal valueLotSmall = new BigDecimal("100");
-      BigDecimal valueLotLarge = new BigDecimal("100");
+		BigDecimal retailPrice = umb01Dto.getPriceRefDto().getRetailPrice();
+		BigDecimal unitPriceSmallParcel = umb01Dto.getPriceRefDto().getUnitPriceSmallParcel();
+		BigDecimal unitPriceForeheadColor = umb01Dto.getPriceRefDto().getUnitPriceForeheadColor();
+		BigDecimal primaryStoreOpenRate = umb01Dto.getPriceRefDto().getPrimaryStoreOpenRate();
+		BigDecimal primaryStoreCommissionAmount = umb01Dto.getPriceRefDto().getPrimaryStoreCommissionAmount();
+		BigDecimal secondStoreOpenRate = umb01Dto.getSecondStoreOpenRate();
+		BigDecimal secondStoreOpenAmount = umb01Dto.getSecondStoreOpenAmount();
+		BigDecimal lotQuantity = umb01Dto.getPriceRefDto().getLotQuantity();
+		BigDecimal calValue = new BigDecimal("0");
+		BigDecimal valueLotSmall = new BigDecimal("100");
+		BigDecimal valueLotLarge = new BigDecimal("100");
 
-      // pattern 1
-      if(!BigDecimal.ZERO.equals(retailPrice) && BigDecimal.ZERO.equals(unitPriceSmallParcel) &&
-          BigDecimal.ZERO.equals(unitPriceForeheadColor) && !BigDecimal.ZERO.equals(primaryStoreOpenRate) &&
-          BigDecimal.ZERO.equals(primaryStoreCommissionAmount) && !BigDecimal.ZERO.equals(secondStoreOpenRate) &&
-          BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
-    	  calValue = retailPrice.subtract(primaryStoreOpenRate.multiply(retailPrice))
-    			  				.subtract(secondStoreOpenRate.multiply(retailPrice));
-      }
-      
-      // pattern 2
-      if(!BigDecimal.ZERO.equals(retailPrice) && 
-          BigDecimal.ZERO.equals(unitPriceSmallParcel) &&
-          BigDecimal.ZERO.equals(unitPriceForeheadColor) &&
-          BigDecimal.ZERO.equals(primaryStoreOpenRate) &&
-          !BigDecimal.ZERO.equals(primaryStoreCommissionAmount) &&
-          BigDecimal.ZERO.equals(secondStoreOpenRate) &&
-          !BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
-    	  calValue = retailPrice.subtract(primaryStoreCommissionAmount).subtract(secondStoreOpenAmount);
-      }
-      
-      // pattern 3
-      if(!BigDecimal.ZERO.equals(retailPrice) && 
-          !BigDecimal.ZERO.equals(unitPriceSmallParcel) &&
-          BigDecimal.ZERO.equals(unitPriceForeheadColor) &&
-          !BigDecimal.ZERO.equals(primaryStoreOpenRate) &&
-          BigDecimal.ZERO.equals(primaryStoreCommissionAmount) &&
-          !BigDecimal.ZERO.equals(secondStoreOpenRate) &&
-          BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
-    	  if (valueLotSmall.compareTo(lotQuantity) < 0) {
-    		  calValue = retailPrice.add(unitPriceSmallParcel)
-    				  				.subtract(primaryStoreOpenRate.multiply(retailPrice.add(unitPriceSmallParcel)))
-    				  				.subtract(secondStoreOpenRate.multiply(retailPrice.add(unitPriceSmallParcel)));
-    	  } else {
-    		  calValue = retailPrice.subtract(primaryStoreOpenRate.multiply(retailPrice))
-		  							.subtract(secondStoreOpenRate.multiply(retailPrice));
-    	  }
-      }
-      
-      // pattern 4
-      if(!BigDecimal.ZERO.equals(retailPrice) && 
-          !BigDecimal.ZERO.equals(unitPriceSmallParcel) &&
-          BigDecimal.ZERO.equals(unitPriceForeheadColor) &&
-          BigDecimal.ZERO.equals(primaryStoreOpenRate) &&
-          !BigDecimal.ZERO.equals(primaryStoreCommissionAmount) &&
-          BigDecimal.ZERO.equals(secondStoreOpenRate) &&
-          !BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
-    	  if (valueLotSmall.compareTo(lotQuantity) < 0) {
-    		  calValue = retailPrice.add(unitPriceSmallParcel)
-    				  				.subtract(primaryStoreCommissionAmount)
-    				  				.subtract(secondStoreOpenAmount);
-    	  } else {
-    		  calValue = retailPrice.subtract(primaryStoreCommissionAmount)
-		  							.subtract(secondStoreOpenAmount);
-    	  }
-      }
-      
-      // pattern 5
-      if(!BigDecimal.ZERO.equals(retailPrice) && 
-          BigDecimal.ZERO.equals(unitPriceSmallParcel) &&
-          !BigDecimal.ZERO.equals(unitPriceForeheadColor) &&
-          !BigDecimal.ZERO.equals(primaryStoreOpenRate) &&
-          BigDecimal.ZERO.equals(primaryStoreCommissionAmount) &&
-          !BigDecimal.ZERO.equals(secondStoreOpenRate) &&
-          BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
-    	  if (valueLotLarge.compareTo(lotQuantity) < 0) {
-    		  calValue = retailPrice.add(unitPriceForeheadColor)
-		  							.subtract(primaryStoreOpenRate.multiply(retailPrice.add(unitPriceForeheadColor)))
-		  							.subtract(secondStoreOpenRate.multiply(retailPrice.add(unitPriceForeheadColor)));
-    	  } else {
-    		  calValue = retailPrice.subtract(primaryStoreOpenRate.multiply(retailPrice))
-									.subtract(secondStoreOpenRate.multiply(retailPrice));
-    	  }
-      }
-      
-      // pattern 6
-      if(!BigDecimal.ZERO.equals(retailPrice) && 
-          BigDecimal.ZERO.equals(unitPriceSmallParcel) &&
-          !BigDecimal.ZERO.equals(unitPriceForeheadColor) &&
-          BigDecimal.ZERO.equals(primaryStoreOpenRate) &&
-          !BigDecimal.ZERO.equals(primaryStoreCommissionAmount) &&
-          BigDecimal.ZERO.equals(secondStoreOpenRate) &&
-          !BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
-    	  if (valueLotLarge.compareTo(lotQuantity) < 0) {
-    		  
-    	  }
-      }
-      
-      // pattern 7
-      if(!BigDecimal.ZERO.equals(retailPrice) && 
-          !BigDecimal.ZERO.equals(unitPriceSmallParcel) &&
-          !BigDecimal.ZERO.equals(unitPriceForeheadColor) &&
-          !BigDecimal.ZERO.equals(primaryStoreOpenRate) &&
-          BigDecimal.ZERO.equals(primaryStoreCommissionAmount) &&
-          !BigDecimal.ZERO.equals(secondStoreOpenRate) &&
-          BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
-    	  
-      }
-      
-      // pattern 8
-      if(!BigDecimal.ZERO.equals(retailPrice) && 
-          !BigDecimal.ZERO.equals(unitPriceSmallParcel) &&
-          !BigDecimal.ZERO.equals(unitPriceForeheadColor) &&
-          BigDecimal.ZERO.equals(primaryStoreOpenRate) &&
-          !BigDecimal.ZERO.equals(primaryStoreCommissionAmount) &&
-          BigDecimal.ZERO.equals(secondStoreOpenRate) &&
-          !BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
-    	  
-      }
-      
+		// pattern 1
+		if (!BigDecimal.ZERO.equals(retailPrice) && BigDecimal.ZERO.equals(unitPriceSmallParcel)
+				&& BigDecimal.ZERO.equals(unitPriceForeheadColor) && !BigDecimal.ZERO.equals(primaryStoreOpenRate)
+				&& BigDecimal.ZERO.equals(primaryStoreCommissionAmount) && !BigDecimal.ZERO.equals(secondStoreOpenRate)
+				&& BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
+			calValue = retailPrice.subtract(primaryStoreOpenRate.multiply(retailPrice))
+					.subtract(secondStoreOpenRate.multiply(retailPrice));
+		}
+	  
+		// pattern 2
+		if (!BigDecimal.ZERO.equals(retailPrice) && BigDecimal.ZERO.equals(unitPriceSmallParcel)
+				&& BigDecimal.ZERO.equals(unitPriceForeheadColor) && BigDecimal.ZERO.equals(primaryStoreOpenRate)
+				&& !BigDecimal.ZERO.equals(primaryStoreCommissionAmount) && BigDecimal.ZERO.equals(secondStoreOpenRate)
+				&& !BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
+			calValue = retailPrice.subtract(primaryStoreCommissionAmount).subtract(secondStoreOpenAmount);
+		}
+
+		// pattern 3
+		if (!BigDecimal.ZERO.equals(retailPrice) && !BigDecimal.ZERO.equals(unitPriceSmallParcel)
+				&& BigDecimal.ZERO.equals(unitPriceForeheadColor) && !BigDecimal.ZERO.equals(primaryStoreOpenRate)
+				&& BigDecimal.ZERO.equals(primaryStoreCommissionAmount) && !BigDecimal.ZERO.equals(secondStoreOpenRate)
+				&& BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
+			if (valueLotSmall.compareTo(lotQuantity) < 0) {
+				calValue = retailPrice.add(unitPriceSmallParcel)
+						.subtract(primaryStoreOpenRate.multiply(retailPrice.add(unitPriceSmallParcel)))
+						.subtract(secondStoreOpenRate.multiply(retailPrice.add(unitPriceSmallParcel)));
+			} else {
+				calValue = retailPrice.subtract(primaryStoreOpenRate.multiply(retailPrice))
+						.subtract(secondStoreOpenRate.multiply(retailPrice));
+			}
+		}
+
+		// pattern 4
+		if (!BigDecimal.ZERO.equals(retailPrice) && !BigDecimal.ZERO.equals(unitPriceSmallParcel)
+				&& BigDecimal.ZERO.equals(unitPriceForeheadColor) && BigDecimal.ZERO.equals(primaryStoreOpenRate)
+				&& !BigDecimal.ZERO.equals(primaryStoreCommissionAmount) && BigDecimal.ZERO.equals(secondStoreOpenRate)
+				&& !BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
+			if (valueLotSmall.compareTo(lotQuantity) < 0) {
+				calValue = retailPrice.add(unitPriceSmallParcel).subtract(primaryStoreCommissionAmount)
+						.subtract(secondStoreOpenAmount);
+			} else {
+				calValue = retailPrice.subtract(primaryStoreCommissionAmount).subtract(secondStoreOpenAmount);
+			}
+		}
+
+		// pattern 5
+		if (!BigDecimal.ZERO.equals(retailPrice) && BigDecimal.ZERO.equals(unitPriceSmallParcel)
+				&& !BigDecimal.ZERO.equals(unitPriceForeheadColor) && !BigDecimal.ZERO.equals(primaryStoreOpenRate)
+				&& BigDecimal.ZERO.equals(primaryStoreCommissionAmount) && !BigDecimal.ZERO.equals(secondStoreOpenRate)
+				&& BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
+			if (valueLotLarge.compareTo(lotQuantity) < 0) {
+				calValue = retailPrice.add(unitPriceForeheadColor)
+						.subtract(primaryStoreOpenRate.multiply(retailPrice.add(unitPriceForeheadColor)))
+						.subtract(secondStoreOpenRate.multiply(retailPrice.add(unitPriceForeheadColor)));
+			} else {
+				calValue = retailPrice.subtract(primaryStoreOpenRate.multiply(retailPrice))
+						.subtract(secondStoreOpenRate.multiply(retailPrice));
+			}
+		}
+
+		// pattern 6
+		if (!BigDecimal.ZERO.equals(retailPrice) && BigDecimal.ZERO.equals(unitPriceSmallParcel)
+				&& !BigDecimal.ZERO.equals(unitPriceForeheadColor) && BigDecimal.ZERO.equals(primaryStoreOpenRate)
+				&& !BigDecimal.ZERO.equals(primaryStoreCommissionAmount) && BigDecimal.ZERO.equals(secondStoreOpenRate)
+				&& !BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
+			if (valueLotLarge.compareTo(lotQuantity) < 0) {
+				calValue = retailPrice.add(unitPriceForeheadColor).subtract(primaryStoreCommissionAmount)
+						.subtract(secondStoreOpenAmount);
+			} else {
+				calValue = retailPrice.subtract(primaryStoreCommissionAmount).subtract(secondStoreOpenAmount);
+			}
+		}
+
+		// pattern 7
+		if (!BigDecimal.ZERO.equals(retailPrice) && !BigDecimal.ZERO.equals(unitPriceSmallParcel)
+				&& !BigDecimal.ZERO.equals(unitPriceForeheadColor) && !BigDecimal.ZERO.equals(primaryStoreOpenRate)
+				&& BigDecimal.ZERO.equals(primaryStoreCommissionAmount) && !BigDecimal.ZERO.equals(secondStoreOpenRate)
+				&& BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
+			if (valueLotSmall.compareTo(lotQuantity) >= 0 && valueLotLarge.compareTo(lotQuantity) < 0) {
+				calValue = retailPrice.add(unitPriceSmallParcel).add(unitPriceForeheadColor)
+						.subtract(primaryStoreOpenRate
+								.multiply(retailPrice.add(unitPriceSmallParcel).add(unitPriceForeheadColor)))
+						.subtract(secondStoreOpenRate
+								.multiply(retailPrice.add(unitPriceSmallParcel).add(unitPriceForeheadColor)));
+			} else {
+				calValue = retailPrice.subtract(primaryStoreOpenRate.multiply(retailPrice))
+						.subtract(secondStoreOpenRate.multiply(retailPrice));
+			}
+		}
+
+		// pattern 8
+		if (!BigDecimal.ZERO.equals(retailPrice) && !BigDecimal.ZERO.equals(unitPriceSmallParcel)
+				&& !BigDecimal.ZERO.equals(unitPriceForeheadColor) && BigDecimal.ZERO.equals(primaryStoreOpenRate)
+				&& !BigDecimal.ZERO.equals(primaryStoreCommissionAmount) && BigDecimal.ZERO.equals(secondStoreOpenRate)
+				&& !BigDecimal.ZERO.equals(secondStoreOpenAmount)) {
+			if (valueLotSmall.compareTo(lotQuantity) < 0) {
+				calValue = retailPrice.add(unitPriceForeheadColor).add(unitPriceSmallParcel)
+						.subtract(primaryStoreCommissionAmount).subtract(secondStoreOpenAmount);
+			} else if (valueLotSmall.compareTo(lotQuantity) >= 0 && valueLotLarge.compareTo(lotQuantity) < 0) {
+				calValue = retailPrice.add(unitPriceForeheadColor).subtract(primaryStoreCommissionAmount)
+						.subtract(secondStoreOpenAmount);
+			} else {
+				calValue = retailPrice.subtract(primaryStoreOpenRate.multiply(retailPrice))
+						.subtract(secondStoreOpenRate.multiply(retailPrice));
+			}
+		}
+
   }
   
   public String getCsvFilePath() {
